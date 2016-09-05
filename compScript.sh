f6fada8b1a9ec5f8dbb381b3aa8edcd4278f3e08
Svensk-Xavier <j.nelssonsmith@gmail.com>
2016-09-05 10:32:47 +1000

    Added a structure

---
+++ compScript.sh
@@ -0,0 +1,6 @@
+# !/bin/sh
+
+echo 'Compiling c program...'
+gcc -o task1 task1.c
+gcc -o task2 task2.c
+echo 'C programs compiled!'
---
+++ task2.c
@@ -0,0 +1,14 @@
+/*
+NAME: Joshua Nelsson-Smith
+STUDENT ID: 25954113
+START DATE: 03/09/16
+LAST MODIFIED:
+DESCRIPTION:
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    return 0;
+}
